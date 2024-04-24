05358140fcb7d929a3404f16f7ae2fdfbffb7be0
rohitkurup <rohitkurup87@gmail.com>
2024-04-24 22:07:46 +0200

    Task1

---
+++ Task 1/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] number = {2,4,7,2,5};
+        int big = -2147483648;
+        int small = 2147483647 ;
+        for (int i=0; i< number.length; i++) {
+
+            //System.out.println(number[i]);
+            if(number[i]>big)
+            {
+                big = number[i];
+            }
+
+        }
+        System.out.println("Big value = " +big);
+        for (int i=0; i< number.length; i++) {
+
+            //System.out.println(number[i]);
+            if(number[i]<small)
+            {
+                small = number[i];
+            }
+
+        }
+        System.out.println("Small value = " + small);
+        System.out.println("Difference = " + (big-small));
+
+    }
+}
